23d2d2b5a7930baea643d0467551101d6c9f3f96
Jim Boswell <jimb@yellcorp.org>
2015-02-15 14:21:33 -0800

    fix wrong format args to error message. figure out a better way to do this!

--- disklib/msfat.py
+++ disklib/msfat.py
@@ -162,7 +162,7 @@ class FATVolume(object):
 		if b.BPB_TotSec16 == 0 and b.BPB_TotSec32 == 0:
 			yield (_INVALID, "Invalid sector count: Both BPB_TotSec16 and BPB_TotSec32 are zero")
 		elif b.BPB_TotSec16 != 0 and b.BPB_TotSec32 != 0:
-			yield (_INVALID, "Invalid sector count: Both BPB_TotSec16 (0x{0:04X}) and BPB_TotSec32 (0x{0:08X}) are nonzero".format(b.BPB_TotSec16, b.BPB_TotSec32))
+			yield (_INVALID, "Invalid sector count: Both BPB_TotSec16 (0x{0:04X}) and BPB_TotSec32 (0x{1:08X}) are nonzero".format(b.BPB_TotSec16, b.BPB_TotSec32))
 
 		if b.BPB_Media not in _VALID_MEDIA_BYTE:
 			yield (_INVALID, "Invalid BPB_Media: 0x{0:02X}".format(b.BPB_Media))
