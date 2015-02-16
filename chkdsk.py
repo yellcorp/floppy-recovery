11a64a7161d3c7490ee889e497aa2e29335e5b86
Jim Boswell <jimb@yellcorp.org>
2015-02-15 18:29:39 -0800

    chkdsk multiple image files

--- chkdsk.py
+++ chkdsk.py
@@ -15,16 +15,31 @@ LOGLEVEL_TO_PREFIX = {
 
 
 def main():
-	path = sys.argv[1]
-	validity = disklib.validity.read_validity_for_file(path)
-	stream = open(path, "rb")
-	geometry = disklib.mediageom.DiskGeometry.from_image_size(validity.domain)
+	prog_errs = [ ]
 
-	volume = disklib.msfat.FATVolume(stream, geometry)
-	for level, message in volume.chkdsk():
-		print LOGLEVEL_TO_PREFIX.get(level, "") + message
+	for path in sys.argv[1:]:
+		print path
 
-	stream.close()
+		try:
+			validity = disklib.validity.read_validity_for_file(path)
+			stream = open(path, "rb")
+			geometry = disklib.mediageom.DiskGeometry.from_image_size(validity.domain)
+
+			volume = disklib.msfat.FATVolume(stream, geometry)
+			for level, message in volume.chkdsk():
+				print LOGLEVEL_TO_PREFIX.get(level, "") + message
+
+			stream.close()
+		except Exception as e:
+			print u"{0}Program error: {1}".format(
+				LOGLEVEL_TO_PREFIX[disklib.msfat.CHKDSK_LOG_INVALID], e)
+			prog_errs.append((path, e))
+		print ""
+
+	if prog_errs:
+		print "Program errors ({0}):".format(len(prog_errs))
+		for path, e in prog_errs:
+			print u"{0}: {1!s}".format(path, e)
 
 
 if __name__ == '__main__':
