c80dba0f1f7915c8248cd7d64abc04334ec81e9a
Jim Boswell <jimb@yellcorp.org>
2015-02-22 19:07:55 -0800

    don't count spaces, let the computer do it

--- msfat/dir.py
+++ msfat/dir.py
@@ -9,8 +9,8 @@ from msfat import ATTR_READ_ONLY, ATTR_HIDDEN, ATTR_SYSTEM, ATTR_VOLUME_ID, \
 	ATTR_RESERVED_MASK
 
 
-THISDIR_NAME = ".          "
-UPDIR_NAME =   "..         "
+THISDIR_NAME = "{0:11s}".format(".")
+UPDIR_NAME =   "{0:11s}".format("..")
 
 
 def allowed_in_short_name(char):
