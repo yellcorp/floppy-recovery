66cc3e91631726b32c2c1514b6393825e4dfd7ad
Jim Boswell <jimb@yellcorp.org>
2015-02-28 16:31:44 -0800

    named values for common floppy disk sizes in bytes

--- disklib/__init__.py
+++ disklib/__init__.py
@@ -1,5 +1,13 @@
 import operator
 
 
+CAPACITY_1_44MB = 1474560
+CAPACITY_2_88MB = 2949120
+CAPACITY_720K = 737280
+
+CAPACITY_1_2MB = 1228800
+CAPACITY_360K = 368640
+
+
 def product(iterable):
 	return reduce(operator.mul, iterable, 1)
